7e1502154c4e4c6b118f479cb166613e5d0455a3
Rory Murray <rory.christian.murray@gmail.com>
2019-05-19 19:24:13 -0400

    added index styling, added team page

---
+++ team.js
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang='en'>
+
+<head>
+    <meta charset="utf-8">
+    <title>Wedding Planner App</title>
+    <link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/index.css"/>
+
+    <script src="js/index.js" async></script>
+    <script src="js/dropdown.js" async></script>
+</head>
+
+<body class="container">
+  
+</body>
+
+</html>
